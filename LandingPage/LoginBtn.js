48debd133d129f0a76475d6633498de4d013b09a
John Maguire <jmaguire5588@gmail.com>
2016-05-10 16:11:57 -0400

    fb login working, now working on getting that information to our backend

--- LandingPage/LoginBtn.js
+++ LandingPage/LoginBtn.js
@@ -5,12 +5,36 @@ import React, {
   StyleSheet,
 } from 'react-native';
 
-import FBLogin from 'react-native-facebook-login'
+import FBLogin from 'react-native-facebook-login';
+import GraphRequest from 'react-native-fbsdk';
+import GraphRequestManager from 'react-native-fbsdk';
 
 class Login extends Component{
+  _responseInfoCallback(error: ?Object, result: ?Object){
+    if (error) {
+      alert('Error fetching data: ' + error.toString());
+    } else {
+      alert('Success fetching data: ' + result.toString());
+    }
+  }
+
   render() {
     return (
-      <FBLogin />
+      <FBLogin
+        onLogin={function(data){
+          fetch('graph.facebook.com', {
+            method: 'GET',
+            body: JSON.stringify({
+              token: "EAAGy8cRtCH0BAFWZAt49NsY7EGDEYXCnfFvKAqkGXwwIce6xlgeFiWGR3xJiI6J2zwR8Eh4IVcS8Q7Da11jwS0iOZB9b9odePeskfD1kUZAkg9648kquuoyMmyYXHljMIE95RZBoWdTznCNRoK4SAAz1WegKGeqJZC4yQssKwzSs9hSOGYjG8CmoLX2au4BIZD",
+              userId: "10101119175185840",
+              fields: 'id,name'
+            })
+          }).then((response) => response.json())
+            .then((responseData) => {console.log(responseData)})
+            .done();
+
+        }}
+      />
     );
   }
 };
